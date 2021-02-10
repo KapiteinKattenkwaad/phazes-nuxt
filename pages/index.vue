08ef5d581dd6b78e90bd9b2dd5456179cac70a8f
Max <maxstouten@hotmail.com>
2021-02-10 23:00:09 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="site-wrapper">
+    <TheHeader>
+    </TheHeader>
+    <main class="main" role="main">
+      <TopSection>
+      </TopSection>
+      <TurnOn>
+      </TurnOn>
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+import TheHeader from "~/components/TheHeader";
+import TopSection from "~/components/TopSection";
+import TurnOn from "~/components/TurnOn";
+
+export default {
+  components: {
+    TurnOn,
+    TopSection,
+    TheHeader
+
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
+
